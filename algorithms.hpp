281f3aa8560d5346911cfc45550102af2a7727ce
gatesyp <ig11@zips.uakron.edu>
2016-04-24 01:14:44 +0000

    working code, fixed bugs

--- algorithms.hpp
+++ algorithms.hpp
@@ -10,31 +10,29 @@ using namespace std;
 // will have MST algos, shortest path algo, and dfs/bfs
 struct algorithm
 {
-	int INT_MAX = 0;
 	// stuff	
 };
 
 
 struct Djisktra : algorithm
 {
-	int getPath(const vector< vector<Edge> > &graph, int source, int target)
-	{
-	vector<int> min_distance( graph.size(), INT_MAX);
-	min_distance[source] = 0;
-	set< pair<int,int> > active_vertices;
-	active_vertices.insert( {0, source} );
-	while (!active_vertices.empty()) {
-		int where = active_vertices.begin()->second;
-		if (where == target) return min_distance[where];
-		active_vertices.erase( active_vertices.begin() );
-		for (auto Edge : graph[where]) 
-			if (min_distance[Edge.to] > min_distance[where] + Edge.length) {
-				active_vertices.erase( { min_distance[Edge.to], Edge.to } );
-				min_distance[Edge.to] = min_distance[where] + Edge.length;
-				active_vertices.insert( { min_distance[Edge.to], Edge.to } );
-			}
-	}
-	return INT_MAX;
+	int dijkstra(const vector< vector<Edge> > &graph, int source, int target) {
+		vector<int> min_distance( graph.size(), INT_MAX );
+		min_distance[ source ] = 0;
+		set< pair<int,int> > active_vertices;
+		active_vertices.insert( {0,source} );
+		while (!active_vertices.empty()) {
+			int where = active_vertices.begin()->second;
+			if (where == target) return min_distance[where];
+			active_vertices.erase( active_vertices.begin() );
+			for (auto Edge : graph[where])
+				if (min_distance[Edge.to] > min_distance[where] + Edge.length) {
+					active_vertices.erase( { min_distance[Edge.to], Edge.to } );
+					min_distance[Edge.to] = min_distance[where] + Edge.length;
+					active_vertices.insert( { min_distance[Edge.to], Edge.to } );
+				}
+		}
+		return INT_MAX;
 	}
 
 };
--- test.cpp
+++ test.cpp
@@ -3,32 +3,17 @@
 #include <set>
 #include <vector>
 #include "graph.cpp"
+#include "algorithms.cpp"
+
 using namespace std;
 // throws Seg Fault when using option 2
 
-int dijkstra(const vector< vector<Edge> > &graph, int source, int target) {
-	vector<int> min_distance( graph.size(), INT_MAX );
-	min_distance[ source ] = 0;
-	set< pair<int,int> > active_vertices;
-	active_vertices.insert( {0,source} );
-	while (!active_vertices.empty()) {
-		int where = active_vertices.begin()->second;
-		if (where == target) return min_distance[where];
-		active_vertices.erase( active_vertices.begin() );
-		for (auto Edge : graph[where])
-			if (min_distance[Edge.to] > min_distance[where] + Edge.length) {
-				active_vertices.erase( { min_distance[Edge.to], Edge.to } );
-				min_distance[Edge.to] = min_distance[where] + Edge.length;
-				active_vertices.insert( { min_distance[Edge.to], Edge.to } );
-			}
-	}
-	return INT_MAX;
-}
 
 int main() {
 	// option 1
 	vector< vector<Edge> > graph = { { {1,1}, {3,2} }, { {2,1} }, { {3,1} }, { } };
-	cout << dijkstra(graph,0,3) << endl;
+	Djisktra algo1;
+	cout << algo1.dijkstra(graph,0,3) << endl;
 
 	// option 2
 	// maybe goes out of scope during algo run, and accesses bad memory? 

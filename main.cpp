a005dcb118a34ac261718eacbe5f52c96afd00dc
gatesyp <ig11@zips.uakron.edu>
2016-04-24 01:48:50 +0000

    seg fault on 2nd iteration

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include "Complexity_Timer.hpp"
+#include "Complexity_Recorder.hpp"
+#include <climits>
 #include <iostream>
+#include <set>
 #include <vector>
 #include "graph.cpp"
 #include "algorithms.cpp"
-#include "Complexity_Timer.hpp"
-#include "Complexity_Recorder.hpp"
 // use  case: 
 //
 // vector< vector<Edge> > adjacancies;
@@ -19,17 +21,45 @@
 // adjacancies[u].size;
 //
 
+
 using namespace std;
+// throws Seg Fault when using option 2
 
-int main()
-{
-	graph myGraph;
-	// create a random graph
-	myGraph.addElement(0, 1, 0);
-	myGraph.addElement(1, 2, 2);
-	myGraph.addElement(0, 2, 1);
-	Djisktra d;
-	int max = d.getPath(myGraph.graph, 0, 2);
-	cout << max;
+timer timer1;
 
+int main() {
+	// option 1
+	vector<recorder<timer> > stats(10);	
+	Djisktra algo1;
+	for(int i = 0; i < 4; ++i) {
+		stats[i].reset();
+	}
+	int counter = 0;
+	for(int SIZE = 100; SIZE < 100000; SIZE *= 10) {
+		cout << SIZE << endl;
+
+		//initialize graph
+		vector< vector<Edge> > graph = { { {1,1}, {3,2} }, { {2,1} }, { {3,1} }, { } };
+		int node;
+		for(int i = 0; i< SIZE; i++) {
+			node = rand() % SIZE;
+			graph[node] = { {rand() % SIZE, rand() % SIZE} };
+		}
+
+		timer1.restart();
+		cout << algo1.dijkstra(graph,0,(graph.size() - 1)) << endl;
+		timer1.stop();
+
+		stats[counter].record(timer1);
+
+		++counter;
+	}
+	// option 2
+	// maybe goes out of scope during algo run, and accesses bad memory? 
+	//graph myGraph;
+	//myGraph.addElement(0, 1, 1);
+	//myGraph.addElement(0, 3, 2);
+	//myGraph.addElement(0, 2, 1);
+	//myGraph.addElement(0, 3, 1);
+	// cout << dijkstra(myGraph.graph,0,3) << endl;
 }

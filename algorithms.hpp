1ff02e9ddccdf6940ca082cac403f732cecdd262
gatesyp <ig11@zips.uakron.edu>
2016-04-27 13:46:17 -0400

    finished djisktra

--- algorithms.hpp
+++ algorithms.hpp
@@ -7,19 +7,19 @@
 #include "graph.hpp"
 using namespace std;
 // define the base algorithm class
-// will have MST algos, shortest path algo, and dfs/bfs
 struct Toolbox
 {
 	vector< vector<Edge> > graph;
+	set< pair<int,int> > active_vertices;
+	int size, max;
+
 	Toolbox() : graph() 
 	{}
-	// graph.size(), INT_MAX 
-	int size, max;
 	Toolbox(int d_size, int d_max) : size(d_size), max(d_max)
 	{}	
 
-	set< pair<int,int> > active_vertices;
-	virtual int algorithm(int source, int target){ return source;}
+	virtual int algorithm(int source, int target){ return source; }
+
 	void createGraph(int choice)
 	{
 		if(choice == 0)
@@ -30,42 +30,49 @@ struct Toolbox
 		}
 		else
 		{
+			graph.resize(5);
 			graph[0].push_back( {1,1});
 			graph[0].push_back({3,2});
 			graph[1].push_back( {3,2});
 			graph[2].push_back( {2,1}); 
 			graph[3].push_back( {3,1});
 			graph[4].push_back( { }) ;
-
-
-
 		}
 	}
-
-
-	// stuff	
 };
-struct Test{};
+
 struct Djisktra : Toolbox 
 {
 	int algorithm(int source, int target) {
 		vector<int> min_distance(graph.size(), INT_MAX);
-		cout << "Djisktra";
 		min_distance[ source ] = 0;
 		active_vertices.insert( {0,source} );
-		while (!active_vertices.empty()) {
+		while (!active_vertices.empty()) 
+		{
 			int where = active_vertices.begin()->second;
-			if (where == target) return min_distance[where];
+			if (where == target) 
+				return min_distance[where];
 			active_vertices.erase( active_vertices.begin() );
+
 			for (auto Edge : graph[where])
-				if (min_distance[Edge.to] > min_distance[where] + Edge.length) {
+				if (min_distance[Edge.to] > min_distance[where] + Edge.length) 
+				{
 					active_vertices.erase( { min_distance[Edge.to], Edge.to } );
 					min_distance[Edge.to] = min_distance[where] + Edge.length;
 					active_vertices.insert( { min_distance[Edge.to], Edge.to } );
 				}
 		}
+
 		return INT_MAX;
 	}
 
+};
+
+struct Floyd : Toolbox
+{
+	int algorithm(int source, int target)
+	{
+		return INT_MAX;
+	}
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -6,50 +6,18 @@
 #include <set>
 #include <vector>
 #include "graph.hpp"
-// use  case: 
-//
-// vector< vector<Edge> > adjacancies;
-//
-//
-// add an edge (u,v) with weight w
-//
-// adjacancies[u].push_back(Edge(v,w));
-//
-//
-// get out-degree of a vertex
-//
-// adjacancies[u].size;
-//
-
 
 using namespace std;
-// throws Seg Fault when using option 2
 
 timer timer1;
+// add in timer logic
 int main() {
-	Test c;
-	//Toolbox tools;
-	Edge e;
-	// option 1
-	vector<recorder<timer> > stats(10);	
-	for(int i = 0; i < 4; ++i) {
-		stats[i].reset();
-	}
-	int counter = 0;
-	// initialize graph space
-	//Edge e;
-	//vector< vector<Edge> > graph(10, vector<Edge>(10));// = { { {1,1}, {3,2} }, { {2,1} }, { {3,1} }, { } };
-	//int SIZE = 10;
-	//cout << SIZE << endl;
+	// completely working Djisktra
+	Djisktra algo;
+	algo.createGraph(4);
+	cout << algo.algorithm(0, 3) << endl;
+	// floyd warshal
+	
 
-	//initialize graph
-	//int node, to, length;
 
-	timer1.restart();
-	//cout << algo1.dijkstra(graph,0,(graph.size() - 1)) << endl;
-	timer1.stop();
-
-	stats[counter].record(timer1);
-
-	++counter;
 }

e02986dcae4d51adeda622b2bcf109c15a8bb38e
gatesyp <ig11@zips.uakron.edu>
2016-05-07 17:26:35 +0000

    fixing bugs

--- algorithms.hpp
+++ algorithms.hpp
@@ -10,23 +10,24 @@ using namespace std;
 struct Toolbox
 {
 	int V = 9;
-	int size, max;
-	vector <int> dist[];	 // The output array. dist[i] will hold the shortest
+	vector <int> dist;	 // The output array. dist[i] will hold the shortest
 	vector < vector <int> > graph;
 	// distance from src to i
 
-	vector <bool> sptSet(); // sptSet[i] will true if vertex i is included in shortest
+	vector <bool> sptSet; // sptSet[i] will true if vertex i is included in shortest
 	// path tree or shortest distance from src to i is finalized
 
-
-	Toolbox(int d_size, int d_max) : size(d_size), max(d_max)
+	Toolbox() : V() 
 	{}	
 
+	Toolbox(int d_size) : V(d_size) 
+	{}	
 	virtual int algorithm(int source, int target){ return source; }
 
 	void createGraph(int choice)
 	{
 		sptSet.resize(V);
+			dist.resize(9);
 		if(choice == 0)
 		{
 			//graph.resize(5);
@@ -35,31 +36,37 @@ struct Toolbox
 		}
 		else
 		{
-			//graph.resize(5);
-			//graph[0].push_back( {1,1});
-			//graph[0].push_back({3,2});
-			//graph[1].push_back( {3,2});
-			//graph[2].push_back( {2,1}); 
-			//graph[3].push_back( {3,1});
-			//graph[4].push_back( { }) ;
+			graph.resize(9);
+			for(auto i : graph)
+				i.resize(9);
+			graph = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+				{4, 0, 8, 0, 0, 0, 0, 11, 0},
+				{0, 8, 0, 7, 0, 4, 0, 0, 2},
+				{0, 0, 7, 0, 9, 14, 0, 0, 0},
+				{0, 0, 0, 9, 0, 10, 0, 0, 0},
+				{0, 0, 4, 0, 10, 0, 2, 0, 0},
+				{0, 0, 0, 14, 0, 2, 0, 1, 6},
+				{8, 11, 0, 0, 0, 0, 1, 0, 7},
+				{0, 0, 2, 0, 0, 0, 6, 7, 0}
+				};
 		}
 	}
 	// A utility function to find the vertex with minimum distance value, from
 	// the set of vertices not yet included in shortest path tree
-	int minDistance(int dist[], bool sptSet[])
+
+	void printGraph()
 	{
-		// Initialize min value
-		int min = INT_MAX, min_index;
+		for(auto i : graph)
+		{
+			for(auto j : i)
+				cout << j << " ";
+			cout << endl;
+		}
 
-		for (int v = 0; v < V; v++)
-			if (sptSet[v] == false && dist[v] <= min)
-				min = dist[v], min_index = v;
 
-		return min_index;
 	}
-
 	// A utility function to print the constructed distance array
-	int printSolution(int dist[], int n)
+	void printSolution()
 	{
 		printf("Vertex Distance from Source\n");
 		for (int i = 0; i < V; i++)
@@ -70,37 +77,44 @@ struct Toolbox
 struct Djisktra : Toolbox 
 {
 
+	int minDistance()
+	{
+		cout << "HELLO";	
+		// Initialize min value
+		int min = INT_MAX, min_index;
 
+		for (int v = 0; v < V; v++)
+			if (sptSet[v] == false && dist[v] <= min)
+				min = dist[v], min_index = v;
+
+		return min_index;
+	}
 
 	// Funtion that implements Dijkstra's single source shortest path algorithm
 	// for a graph represented using adjacency matrix representation
-	void dijkstra()
+	void djisktra(int src)
 	{
-		 //[V][V];
-		int src;
 
 		// Initialize all distances as INFINITE and stpSet[] as false
 		for (int i = 0; i < V; i++)
 		{
-			for (int j = 0; j < V; j++)
-			{
-				dist[i].push_back(INT_MAX);
-				sptSet[i].push_back(false);
-			}
+				dist[i] = INT_MAX;
+				sptSet[i] = false;
 		}
 
 		// Distance of source vertex from itself is always 0
-		dist[src][src] = 0;
+		dist[src] = 0;
 
+			cout << "calling min distance";
 		// Find shortest path for all vertices
 		for (int count = 0; count < V-1; count++)
 		{
 			// Pick the minimum distance vertex from the set of vertices not
 			// yet processed. u is always equal to src in first iteration.
-			//int u = minDistance(dist, sptSet); // TODO fix function call
+			int u = minDistance(); // TODO fix function call
 
 			// Mark the picked vertex as processed
-			//sptSet[u][u] = true;
+			sptSet[u] = true;
 
 			// Update dist value of the adjacent vertices of the picked vertex.
 			for (int v = 0; v < V; v++)
@@ -108,13 +122,13 @@ struct Djisktra : Toolbox
 				// Update dist[v] only if is not in sptSet, there is an edge from 
 				// u to v, and total weight of path from src to v through u is 
 				// smaller than current value of dist[v]
-				//if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-						//&& dist[u]+graph[u][v] < dist[v])
-					//dist[v] = dist[u] + graph[u][v];
+				if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
+						&& dist[u]+graph[u][v] < dist[v])
+					dist[v] = dist[u] + graph[u][v];
 		}
 
 		// print the constructed distance array
-		//printSolution(dist, V); // TODO fix function call
+		printSolution(); 
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -13,17 +13,10 @@ timer timer1;
 // add in timer logic
 int main() {
 	int V = 9;
-	/* Let us create the example graph discussed above */
-	//int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
-		//{4, 0, 8, 0, 0, 0, 0, 11, 0},
-		//{0, 8, 0, 7, 0, 4, 0, 0, 2},
-		//{0, 0, 7, 0, 9, 14, 0, 0, 0},
-		//{0, 0, 0, 9, 0, 10, 0, 0, 0},
-		//{0, 0, 4, 0, 10, 0, 2, 0, 0},
-		//{0, 0, 0, 14, 0, 2, 0, 1, 6},
-		//{8, 11, 0, 0, 0, 0, 1, 0, 7},
-		//{0, 0, 2, 0, 0, 0, 6, 7, 0}
-	//};
+	Djisktra d;
+	d.createGraph(2);
+	//d.printGraph();
+	d.djisktra(0);
 
 
 	return 0;
